89e316a00ea07ec03378d1dca0a59c0d4f98df20
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-09-08 16:56:02 +0200

    all episodes fetched from api

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { async } from "q";
 
 export default function App(): JSX.Element {
   const { state, dispatch } = React.useContext(Store);
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
   const fetchDataAction = async () => {
     const url =
       "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
@@ -18,6 +21,23 @@ export default function App(): JSX.Element {
     <React.Fragment>
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode</p>
+      <section>
+        {state.episodes.map((episode: any) => {
+          return (
+            <section key={episode.id}>
+              <img
+                src={episode.image.medium}
+                alt={`Rick and Morty ${episode.name}`}
+              />
+              <div>{episode.name}</div>
+              <section>
+                Season: {episode.season}
+                Number: {episode.number}
+              </section>
+            </section>
+          );
+        })}
+      </section>
     </React.Fragment>
   );
 }
