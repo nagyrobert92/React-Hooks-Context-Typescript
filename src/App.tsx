fa3f723e75e06f9d8e3e0a3ec8fa00e6c035a209
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-09-08 16:42:03 +0200

    useReducer, fetch created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Store } from "./Store";
+import { async } from "q";
 
 export default function App(): JSX.Element {
-  const store = React.useContext(Store);
+  const { state, dispatch } = React.useContext(Store);
+  const fetchDataAction = async () => {
+    const url =
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
+    const data = await fetch(url);
+    const dataJSON = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes
+    });
+  };
   return (
     <React.Fragment>
-      {console.log(store)}
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episode</p>
     </React.Fragment>
