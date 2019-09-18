f84717ce806be237e4da542e0d3968470c94f538
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-09-18 22:56:26 +0200

    reach router types error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Store } from "./Store";
 import { IAction, IEpisode, IEpisodeProps } from "./interfaces";
+import { Link } from "@reach/router";
 
 const EpisodeList = React.lazy<any>(() => import("./EpisodesList"));
 
@@ -48,7 +49,10 @@ export default function App(): JSX.Element {
           <h1>Rick and Morty</h1>
           <p>Pick your favorite episode</p>
         </div>
-        <div>Favourite(s): {state.favourites.length}</div>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/faves"> Favourite(s): {state.favourites.length}</Link>
+        </div>
       </header>
       <React.Suspense fallback={<div>loading...</div>}>
         <section className="episode-layout">
