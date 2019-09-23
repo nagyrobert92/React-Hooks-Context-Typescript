428514a8b8ee53774cae3429e54d78f72e67ef22
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-09-23 16:52:46 +0200

    types corrected

--- src/FavPage.tsx
+++ src/FavPage.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Store } from "./Store";
+import { IEpisodeProps } from "./interfaces";
+import { toggleFavAction } from "./Actions";
 
-export default function FavPage() {
-  return <div>test</div>;
+const EpisodeList = React.lazy<any>(() => import("./EpisodesList"));
+export default function FavPage(): JSX.Element {
+  const { state, dispatch } = React.useContext(Store);
+
+  const props: IEpisodeProps = {
+    episodes: state.favourites,
+    store: { state, dispatch },
+    toggleFavAction,
+    favourites: state.favourites
+  };
+  return (
+    <React.Suspense fallback={<div>loading...</div>}>
+      <div className="episode-layout">
+        <EpisodeList {...props} />
+      </div>
+    </React.Suspense>
+  );
 }
